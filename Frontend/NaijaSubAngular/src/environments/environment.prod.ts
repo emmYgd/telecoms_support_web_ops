ab793160253e25348f8bd80f1cce8d8400a4a735
Adediji Emmanuel Damilare <emmdammy@gmail.com>
2021-02-16 13:15:39 +0100

    Update environment.prod.ts

--- Frontend/NaijaSubAngular/src/environments/environment.prod.ts
+++ Frontend/NaijaSubAngular/src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
     production: true,
     api_url: 'https://api.naijasub.com/api/',
-    external_api: 'https://api.naijasub.com/files/',
-    flutter_wave: 'FLWPUBK-b5111abff41e6f46ac86b0778f7c4c6c-X',
-    providus_api_key: 'MK_PROD_RREU6VSZQE',
-    providus_contract: '9185873168',
+    external_api: '',
+    flutter_wave: '',
+    providus_api_key: '',
+    providus_contract: '',
     app_name: 'Naijasub',
     app_url: 'https://app.naijasub.com/'
 };
